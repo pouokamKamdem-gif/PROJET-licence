cbf459da27e7128fe19a50c2b3fdb2c9be138006
pouokamKamdem-gif <pouokamjoyce@gmail.com>
2026-06-26 00:31:19 +0200

    Add files via upload

---
+++ src/main/java/com/digneequipe/hardoize_backend/HardoizeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.digneequipe.hardoize_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardoizeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardoizeBackendApplication.class, args);
+	}
+
+}
